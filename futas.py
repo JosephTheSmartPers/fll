6eba91ae7ae3eac6824ee450d0f150661e0c551b
JosephTheSmartPers <kupakovacsdummy@gmail.com>
2023-01-13 18:02:52 +0100

    stuff

--- futas.py
+++ futas.py
@@ -22,57 +22,68 @@ from turn import *
 from straight import *
 from reset import *
 
-reset(yhand)
-reset(xhand)
+#Kiszámolja, hogy milyen
 
-straight(2.7, 60, int(gs.angle), 1.5, 20)
+def startCalc(startDegree, plusDegree):
+    return((startDegree - gs.angle) + plusDegree)
+
+"""
+#Visszaállítja a kezdeti pozicóba a karokat
+reset(yhand, 2.5)
+reset(xhand, 1.1)
+"""
+
+startDegree = gs.angle
+
+straight(2.74, 30, int(gs.angle), 1.1, 20)
 xhand.on_for_rotations(90, -1)
-straight(1.8, -60, int(gs.angle), 1.5, 20)
+straight(1.8, -30, int(gs.angle), 1.1, 20)
 yhand.on_for_rotations(90, 0.4)
-straight(2.14, 60, int(gs.angle), 1.5, 20)
-yhand.on_for_rotations(90, 0.8)
+straight(2.15, 30, int(gs.angle) + -1.4, 1.1, 20)
+yhand.on_for_rotations(90, 0.9)
 xhand.on_for_rotations(90, 0.7)
 
 
-time.sleep(0.5) #Wait for cell to rool back
-turn(60, 55, 0.4, 0.5)
-xhand.on_for_rotations(-90, 0.6)
+time.sleep(0.6) #Wait for cell to rool back
 
-yhand.on(2)
-
-print(followLine(0.4, 2.5, 25, 20, rs, True))
-yhand.stop()
-reset(yhand)
-
+turn(startCalc(startDegree, 100), 27, 0.4, 0.5)
 gs.reset()
-straight(1.29, -30, int(gs.angle), 1.5, 20)
+straight(0.2, 25, int(gs.angle), 1, 20)#, True, True)
+turn(startCalc(startDegree, -62), 20, 0.5, 0.3)
+"""
+xhand.on_for_rotations(-90, 0.7)
+yhand.on_for_rotations(90, 0.2)
+print(followLine(0.55, 2.2, 20, 40, ls))
+
+#Innen
+#straight(3.8, 60, int(gs.angle), 1, 20)
+
+yhand.on_for_rotations(45, -1.2)
+m.on_for_rotations(-15, -15, 0.2)
+straight(1.5, -15, int(gs.angle), 1.5, 20)
 
 raiseHeight = 1.1
+raiseSpeed = 45
 
-xhand.on_for_rotations(90, 0.35)
-yhand.on_for_rotations(90, raiseHeight)
-yhand.on_for_rotations(90, -raiseHeight)
-yhand.on_for_rotations(90, raiseHeight)
-yhand.on_for_rotations(90, -raiseHeight)
-yhand.on_for_rotations(90, raiseHeight)
-yhand.on_for_rotations(90, -(raiseHeight / 2))
-xhand.on_for_rotations(90, -0.35)
-straight(1, -60, int(gs.angle) + 60, 1.5, 20)
-turn(30, 55, 0.7, 0.5)
-yhand.on_for_rotations(-90, 0.1)
-xhand.on_for_rotations(90, 0.7)
-straight(6, -90, int(gs.angle), 1.5, 20)
+xhand.on_for_rotations(raiseSpeed, 0.35)
+yhand.on_for_rotations(raiseSpeed, raiseHeight)
+yhand.on_for_rotations(raiseSpeed, -raiseHeight)
+yhand.on_for_rotations(raiseSpeed, raiseHeight)
+yhand.on_for_rotations(raiseSpeed, -raiseHeight)
+yhand.on_for_rotations(raiseSpeed, raiseHeight)
+yhand.on_for_rotations(raiseSpeed, -(raiseHeight / 2))
+xhand.on_for_rotations(raiseSpeed, -0.35)
+straight(1, -30, int(gs.angle) + 60, 1.5, 20)
+turn(30, 25, 0.7, 0.4)
+yhand.on_for_rotations(-45, 0.1)
+#xhand.on_for_rotations(90, 0.2)
+straight(1.1, -45, int(gs.angle), 1.5, 20)
 
-"""
-turn(34, 70, 0.5, 1)
-turn(-45, 70, 0.5, 1)
+turn(45, 35, 0.5, 1)
 
+straight(3, -37.5, int(gs.angle), 1.5, 20)
 
-straight(0.5, -30, int(gs.angle), 1.5, 20)
-
-turn(45, 70, 0.5, 1)
 
 xhand.reset()
-yhand.reset()
-"""
-
+yhand.reset()"""
+#HA KÉSZ VAN ÁRON MEGMUTAT FOTÓT
