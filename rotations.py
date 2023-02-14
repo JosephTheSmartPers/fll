374aae1a0028258e062148ca2e3023a31c363726
JosephTheSmartPers <110565842+JosephTheSmartPers@users.noreply.github.com>
2023-02-14 07:08:50 +0100

    Delete rotations.py

--- rotations.py
+++
@@ -1,7 +0,0 @@
-from ev3dev2.sensor.lego import * 
-from ev3dev2.sensor import *
-from ev3dev2.motor import *
-yhand = MediumMotor(OUTPUT_A)
-xhand = MediumMotor(OUTPUT_D)
-yhand.reset()
-yhand.on_for_rotations(90, )
