32adbd0a562ab26e3fe2cd81e59e0dd1e0cfab16
JosephTheSmartPers <110565842+JosephTheSmartPers@users.noreply.github.com>
2023-02-14 07:08:15 +0100

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from time import time
-from ev3dev2.motor import OUTPUT_A, OUTPUT_D, MediumMotor
-import time
-print("ready")
-m = MediumMotor(OUTPUT_A)
-
-m.on_for_rotations(70, 0.4, False, False)
-m.off(False)
